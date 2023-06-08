c9ba9d2fb27bf597d1853d79a25cb248f14a4907
ChouWong <wongcharlesdave@gmail.com>
2023-06-08 19:38:50 +0800

    User Info

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -63,13 +63,17 @@ class LoginController extends Controller
         try {
 
             $request->validate([
-                'name' => ['required', 'string', 'max:255'],
+                'first_name' => ['required', 'string', 'max:255'],
+                'last_name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'password' => ['required', 'confirmed', Rules\Password::defaults()],
             ]);
 
             $user = User::create([
-                'name' => $request->name,
+                'name' => $request->first_name . ' ' . $request->middle_name . ' ' . $request->last_name,
+                'first_name' => $request->first_name,
+                'middle_name' => $request->middle_name,
+                'last_name' => $request->last_name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
             ]);
