16b89c02b6fffc4dcbc888f84cd98316e67090ec
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-09-16 21:22:20 +0800

    asda

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
         return $next($request)
         ->header("Access-Control-Allow-Origin", "*")
-        ->header("Access-Control-Allow-Methods", "*")
+        ->header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET, OPTIONS")
         ->header("Access-Control-Allow-Headers", "*")
         ;
     }
