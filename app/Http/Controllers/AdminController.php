fcc140ffa38b9051118c28ac20f01a7223fa7e85
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-09-20 18:32:56 +0800

    sdad

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,12 +18,22 @@ class AdminController extends Controller
             $allUserCount = User::count();
             $userCount = User::where('type', 0)->count();
             $ownerCount = User::where('type', 1)->count();
-            $allUserByDateCount = User::select(DB::raw('DATE_FORMAT(`created_at`, "%Y-%m-%d") as created_date'), DB::raw('count(*) as total'))->groupBy(DB::raw('DATE_FORMAT(`created_at`, "%Y-%m-%d")'))->get();
+
+            $allUserByDateCount = User::select(
+                    DB::raw('DATE_FORMAT(`created_at`, "%Y-%m-%d") as created_date'), 
+                    DB::raw('count(*) as total'))
+                ->groupBy(DB::raw('DATE_FORMAT(`created_at`, "%Y-%m-%d")'))
+                ->get();
+
             $allResortCount = Resorts::count();
             $allActiveResortCount = Resorts::where('is_for_rent', 1)->count();
             $allInactiveResortCount = Resorts::where('is_for_rent', 0)->count();
             $capturedResortCount = Resorts::where('capture_status', 1)->count();
-            $allResortByDateCount = Resorts::select(DB::raw('DATE_FORMAT(`created_at`, "%Y-%m-%d") as created_date'), DB::raw('count(*) as total'))->groupBy('created_at')->get();
+            $allResortByDateCount = Resorts::select(
+                    DB::raw('DATE_FORMAT(`created_at`, "%Y-%m-%d") as created_date'), 
+                    DB::raw('count(*) as total'))
+                ->groupBy(DB::raw('DATE_FORMAT(`created_at`, "%Y-%m-%d")'))
+                ->get();
 
             event(new AdminEvent([
                 'allUserCount' => $allUserCount,
