5869cc6b786a82d15a750847a973e89b55ee221d
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-10-01 11:10:46 +0800

    refactor

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -59,7 +59,7 @@ class NotificationController extends Controller
 
 			Notification::insert([
 				'resort_id' => $request->resort_id,
-				'reservation_id' => $reservation_id->id,
+				'reservation_id' => $reservation_id->id ?? null,
 				'user_id' => $request->user_id,
 				'message' => $request->message,
 				'type' => $request->type,
