188396920cd5d63713fd6c2d4d5c230a4ec2c452
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-10-26 01:09:06 +0800

    dsa

--- app/Http/Controllers/ResortController.php
+++ app/Http/Controllers/ResortController.php
@@ -110,11 +110,11 @@ class ResortController extends Controller
                 throw new \Exception("Image is required", 1);
             }
 
-            foreach($request->amenities as $key=>$amenitiesTitle) {
+            foreach($request->amenities as $row) {
                 // CREATE AMENITIES
                 DB::table('resort_amenities')->insert([
                     'resort_id' => $resort,
-                    'description' => $amenitiesTitle['amenitiesTitle'],
+                    'description' => $row['amenitiesTitle'],
                     'created_at' => now(),
                     'created_by' => Auth()->User()->id
                 ]);
