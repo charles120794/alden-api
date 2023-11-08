78085ed6657e1ce9ebc28e638a474dc3b3f83f2b
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-08 12:13:46 +0800

    da

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -32,6 +32,7 @@ class ReservationController extends Controller
             $reservation_list = Reservation::with('userCreated', 'resortInfo.createdUser', 'priceInfo')->get();
             $review_list = ResortRatings::with('createdUser', 'resortInfo')->where('resort_owner_id', auth()->id())->orderBy('created_at', 'desc')->get();
             $reservations_chart = DB::table('resort_reservation')
+                                        ->where('resort_owner_id', auth()->id())
                                         ->join('resort', 'resort.id', '=', 'resort_reservation.resort_id')
                                         ->select('resort_name as name', DB::raw('count(*) as value'))
                                         ->groupBy('resort_name')
