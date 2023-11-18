4f2cab23c4d76e639dd0e1b7840d345c7fec3f92
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-18 21:34:57 +0800

    dsa

--- routes/web.php
+++ routes/web.php
@@ -54,23 +54,23 @@ Route::get('/dashboard', function () {
 
 
 
-// The Email Verification Notice
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+// // The Email Verification Notice
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
 
 
-// // The Email Verification Handler
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
+// // // The Email Verification Handler
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
  
-    return response()->json(['response'=>'completed']);
-})->middleware(['auth', 'signed'])->name('verification.verify');
+//     return response()->json(['response'=>'completed']);
+// })->middleware(['auth', 'signed'])->name('verification.verify');
 
 
-// // Resending The Verification Email
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
+// // // Resending The Verification Email
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
  
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+//     return back()->with('message', 'Verification link sent!');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
