9af8d6672e5ae0621d5335c9ae584685a64054ed
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-21 00:06:58 +0800

    dsa

--- config/mail.php
+++ config/mail.php
@@ -92,7 +92,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'quickrent.online@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'QuickRent'),
     ],
 
     /*
