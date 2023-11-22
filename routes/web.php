9264b7326c6fc5fb95cc82eaa102e4ca86b860de
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-22 21:55:39 +0800

    dsa

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::get('/email/verify', function () {
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
  
-    return redirect('https://quickrent.online/signin');
+    return response()->json(['response'=>'completed']);
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 
