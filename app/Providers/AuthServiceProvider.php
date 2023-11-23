4e1e88aec2073a2ef3d06eedd10bbf11f7610f6d
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-23 23:51:57 +0800

    sdad

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,17 +28,17 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             
             return (new MailMessage)
                 ->subject('Verify Email Address')
                 ->line('Click the button below to verify your email address.')
                 ->action('Verify Email Address', $url);
+                
         });
 
-        ResetPassword::createUrlUsing(function ($user, string $token) {
-            return 'https://quickrent.online/reset-password?token='.$token;
-        });
+        // ResetPassword::createUrlUsing(function ($user, string $token) {
+        //     return 'https://quickrent.online/reset-password?token='.$token;
+        // });
     }
 }
