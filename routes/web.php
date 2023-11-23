14f81a588a11f5f2258085ed80a7a5e6f452f08a
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-23 15:02:16 +0800

    da

--- routes/web.php
+++ routes/web.php
@@ -58,7 +58,11 @@ Route::get('/email/verify', function () {
 
 
 // The Email Verification Handler
-Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+
+        return redirect('/home');
+    })
     ->middleware(['auth', 'signed'])
     ->name('verification.verify');
 
