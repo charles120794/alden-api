aa20aa93de189164bdedd88b617186f256a0373a
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-23 17:09:02 +0800

    da

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,7 @@ Route::get('/email/verify', function () {
 
 
 // The Email Verification Handler
-Route::get('/email/verify/{id}/{hash}', function (\Illuminate\Http\Request $request) {
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
         $request->fulfill();
 
         return redirect('https://quickrent.online/signin');
