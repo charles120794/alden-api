f463f84365596abca47b2b36de09baa96e631db5
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-29 20:12:45 +0800

    da

--- app/Http/Controllers/ResortController.php
+++ app/Http/Controllers/ResortController.php
@@ -125,7 +125,7 @@ class ResortController extends Controller
                 if(json_last_error() === JSON_ERROR_NONE){
                     DB::table('resort_amenities')->insert([
                         'resort_id' => $resort,
-                        'description' => $item["amenitiesTitle"],
+                        'description' => $row["amenitiesTitle"],
                         'created_at' => now(),
                         'created_by' => Auth()->User()->id
                     ]);
