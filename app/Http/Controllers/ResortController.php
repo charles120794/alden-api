fda47d7391a3df87a3436e515be90a07269783be
Charles Dave Wong <wongcharlesdave@gmail.com>
2023-11-29 21:54:14 +0800

    da

--- app/Http/Controllers/ResortController.php
+++ app/Http/Controllers/ResortController.php
@@ -117,22 +117,22 @@ class ResortController extends Controller
             // ]);
 
 
-            foreach ($request->amenities as $row) {
-                // Ensure $row is an array before accessing its elements
-                    // Check if 'amenitiesTitle' exists in the current object
-                // if (isset($row['amenitiesTitle'])) {
-                //     // CREATE AMENITIES
-                //     DB::table('resort_amenities')->insert([
-                //         'resort_id'   => $resort,
-                //         'description' => $row['amenitiesTitle'],
-                //         'created_at'  => now(),
-                //         'created_by'  => Auth()->user()->id,
-                //     ]);
-                // } else {
-                //     throw new \Exception("AmenitiesTitle not found", 1);
-                // }
-                return $row;
-            }
+            // foreach ($request->amenities as $row) {
+            //     // Ensure $row is an array before accessing its elements
+            //         // Check if 'amenitiesTitle' exists in the current object
+            //     if (isset($row['amenitiesTitle'])) {
+            //         // CREATE AMENITIES
+            //         DB::table('resort_amenities')->insert([
+            //             'resort_id'   => $resort,
+            //             'description' => $row['amenitiesTitle'],
+            //             'created_at'  => now(),
+            //             'created_by'  => Auth()->user()->id,
+            //         ]);
+            //     } else {
+            //         throw new \Exception("AmenitiesTitle not found", 1);
+            //     }
+            // }
+            return $request->amenities;
         
             // foreach($request->policies as $row) {
             //     // CREATE POLICIES
